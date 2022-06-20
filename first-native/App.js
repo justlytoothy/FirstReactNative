03d4b80492b0303cb94be52671f0593199f87ffd
Jake Schilling <jake@schillingcentral.net>
2022-06-20 10:59:10 -0600

    redux example

--- first-native/App.js
+++ first-native/App.js
@@ -2,25 +2,14 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import routes from './routes.js'
-
-const Stack = createNativeStackNavigator()
-
+import Navigation from './src/navigation/Navigation.js'
+import { Provider } from 'react-redux'
+import store from './src/redux/store.js'
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				{routes.map((route, index) => {
-					return (
-						<Stack.Screen
-							key={index}
-							name={route.name}
-							component={route.component}
-						/>
-					)
-				})}
-			</Stack.Navigator>
-		</NavigationContainer>
+		<Provider store={store}>
+			<Navigation></Navigation>
+		</Provider>
 	)
 }
 
--- first-native/src/screens/PageTwo.js
+++ first-native/src/screens/PageTwo.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 
-function PageTwo(props) {
+function PageTwo({ navigation }) {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text>Page Two</Text>
